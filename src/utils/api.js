4630dd21127c5e03d3a9496e5a21a25aea9ed802
Kim Yee <kimberly.yee08@gmail.com>
2026-03-05 12:48:24 -0800

    changes not pushed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
-const baseUrl = "http://localhost:3001";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.chanchanwtwr.twilightparadox.com"
+    : "http://localhost:3001";
 export const checkServerResponse = (res) => {
   if (res.ok) {
     return res.json();
